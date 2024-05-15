23d248d542d21f5f826f6e0a3c418e2f0f7e4d86
Hillel1408 <etemax@bk.ru>
2024-05-15 12:53:44 +0300

    меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -90,3 +90,51 @@ const swiperProduct = new Swiper(".product-2__slider", {
         },
     },
 });
+
+const askQuestionBtn = document.querySelector(".ask-question-btn");
+const askQuestionModal = document.querySelector(".ask-question-modal");
+const overlay = document.querySelector(".overlay");
+const askQuestionCloseBtn = document.querySelector(".ask-question-close-btn");
+
+const toggle = (block) => {
+    block.classList.toggle("active");
+    document.body.classList.toggle("lock");
+    overlay.classList.toggle("active");
+};
+
+askQuestionBtn.addEventListener("click", () => {
+    toggle(askQuestionModal);
+});
+askQuestionCloseBtn.addEventListener("click", () => {
+    toggle(askQuestionModal);
+});
+
+const callingAgentBtn = document.querySelector(".calling-agent-btn");
+const callingAgentModal = document.querySelector(".calling-agent-modal");
+const callingAgentCloseBtn = document.querySelector(".calling-agent-close-btn");
+
+callingAgentBtn.addEventListener("click", () => {
+    toggle(callingAgentModal);
+});
+callingAgentCloseBtn.addEventListener("click", () => {
+    toggle(callingAgentModal);
+});
+
+const headerNavItems = document.querySelectorAll(".header__nav > li");
+const headerNav = document.querySelector(".header__block-3");
+const header = document.querySelector(".header");
+
+headerNavItems.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        if (window.innerWidth > 600) {
+            const ul = item.querySelector("ul");
+            ul.style.top = headerNav.clientHeight + 0.2 + "px";
+            overlay.classList.add("active");
+            overlay.style.top = header.clientHeight + 0.2 + "px";
+            overlay.style.height = document.body.scrollHeight + "px";
+        }
+    });
+    item.addEventListener("mouseout", () => {
+        window.innerWidth > 600 && overlay.classList.remove("active");
+    });
+});
