a98e15cc726ecc38986701ff192c19205304ad82
Hillel1408 <etemax@bk.ru>
2024-05-16 11:54:41 +0300

    доработки

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,29 +1,3 @@
-const swiperFuneralArrangements = new Swiper(".funeral-arrangements__slider", {
-    slidesPerView: 2,
-    spaceBetween: 16,
-    navigation: {
-        nextEl: ".funeral-arrangements__slider__button-next",
-        prevEl: ".funeral-arrangements__slider__button-prev",
-    },
-    breakpoints: {
-        666: {
-            slidesPerView: 3,
-        },
-    },
-});
-const swiperCoffinsMoscow = new Swiper(".coffins-moscow__slider", {
-    slidesPerView: 2,
-    spaceBetween: 16,
-    navigation: {
-        nextEl: ".coffins-moscow__slider__button-next",
-        prevEl: ".coffins-moscow__slider__button-prev",
-    },
-    breakpoints: {
-        666: {
-            slidesPerView: 3,
-        },
-    },
-});
 const swiperProduct = new Swiper(".product-2__slider", {
     slidesPerView: 2,
     spaceBetween: 16,
