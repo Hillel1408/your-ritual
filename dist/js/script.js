62de0e3b2c297b20ce60b62df4b1794d1de02429
Hillel1408 <etemax@bk.ru>
2024-05-16 12:01:36 +0300

    доработки

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,3 @@
-const swiperProduct = new Swiper(".product-2__slider", {
-    slidesPerView: 2,
-    spaceBetween: 16,
-    navigation: {
-        nextEl: ".product-2__slider__button-next",
-        prevEl: ".product-2__slider__button-prev",
-    },
-    breakpoints: {
-        666: {
-            slidesPerView: 3,
-        },
-    },
-});
-
 const askQuestionBtn = document.querySelector(".ask-question-btn");
 const askQuestionModal = document.querySelector(".ask-question-modal");
 const overlay = document.querySelector(".overlay");
